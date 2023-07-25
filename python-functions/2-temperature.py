0641ee307fcd28cebd83cf14e463eed83a445b32
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-07-26 01:15:27 +0300

    Update 2-temperature.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,4 @@
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
-    fahrenheit_temperature = float(input("Enter the temperature in Fahrenheit: "))
-    celsius_temperature = convert_to_celsius(fahrenheit_temperature)
-print("Temperature in Celsius:", celsius_temperature)
+print(convert_to_celsius(100))
