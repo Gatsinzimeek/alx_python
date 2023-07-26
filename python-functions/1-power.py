f76750f7f2c682ae543baf5e39aa2ce47b60e498
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-07-26 12:46:20 +0300

    power number

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,5 +1,8 @@
-def powe(a,b):
-    for i in range(b):
-        powers = a * a
-        continue
-    return powers
+def pow(a,b):
+    if b==0:
+        return 1
+    else:
+        Expon = 1
+        for _ in range(b):
+            Expon *= a
+        return Expon
