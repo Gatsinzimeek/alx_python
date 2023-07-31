9dbb4e7cb910d9695600c62a9b7379635f1d0abb
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-07-31 12:34:02 +0300

    working with dynamic

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,12 @@
 import sys
 
-count = 0
+if __name__ == "__main__":
+    count = 0
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(sys.argv)))
-    for i in sys.argv:
-        count += 1
-        print("{}: {}".format(count,i))
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+        for i in sys.argv:
+            count += 1
+            print("{}: {}".format(count,i))
