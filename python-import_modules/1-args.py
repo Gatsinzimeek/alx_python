dd1b25f72cbb51fb0ca1bc3e4d066887e9381bd4
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-07-31 13:43:19 +0300

    works on dynamic file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,12 @@
 import sys
 
+
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
+    arguments = sys.argv[1:] 
+
     num_arguments = len(arguments)
-    if len(sys.argv) == 1:
-        print("0 arguments.")
-    else:
-        print("{} argument:".format(num_arguments))
+    plural = 's' if num_arguments != 1 else ''
+    print(f"Number of argument{plural}: {num_arguments}:")
+    if num_arguments > 0:
         for i, arg in enumerate(arguments, 1):
             print(f"{i}: {arg}")
