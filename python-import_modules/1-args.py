a5ff5b17fa017a7994e5907f40c89cef7f8074d8
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-07-31 13:48:58 +0300

    working with dynamic

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,9 +1,7 @@
 import sys
 
-
 if __name__ == "__main__":
-    arguments = sys.argv[1:] 
-
+    arguments = sys.argv[1:]  
     num_arguments = len(arguments)
     plural = 's' if num_arguments != 1 else ''
     print(f"Number of argument{plural}: {num_arguments}:")
