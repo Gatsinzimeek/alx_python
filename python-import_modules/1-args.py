f2411e8833ca537d5ce036998283df97f12f2d55
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-07-31 13:24:21 +0300

    works on dynamic file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
     if len(sys.argv) == 1:
-        print("0 argument.")
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(num_arguments))
+        print("{} argument:".format(num_arguments))
         for i, arg in enumerate(arguments, 1):
             print(f"{i}: {arg}")
