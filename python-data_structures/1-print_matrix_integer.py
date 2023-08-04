6108aa5b5607e53c27aa17a7624f299bea4a51cb
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-08-04 23:21:50 +0300

    Matrix program

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,7 +2,7 @@ def print_matrix_integer(matrix=[[]]):
     sign = "$"
     for i in matrix:
         for index,j in enumerate(i):
-            print(j, end='')
+            print('{}'.format(j), end='')
             if index < len(i) - 1:
                 print(" ", end='')
         print(sign)
