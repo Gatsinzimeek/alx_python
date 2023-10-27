0f0eb18c61f18a092178b45d3432439301f1ab5c
Gatsinzimeek <hackergastinzi@gmail.com>
2023-10-28 00:04:40 +0200

    changed again

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,8 +1,18 @@
 """
 This is a module for performing instance checking 
 """
-# This is function use to check if instate of class
 def is_same_class(obj, a_class):
+    """
+    checking if an object recieved is and instance of a_class
+
+    Parameters:
+    obj : The number.
+    a_class: the class.
+
+    Returns:
+    True if is an instance 
+    False if is not and instance
+    """
     if isinstance(obj,a_class):
         return True
     else:
