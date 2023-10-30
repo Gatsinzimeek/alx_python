8d894fd1de6a6225364dda429c8abf47c653c5e7
Gatsinzimeek <hackergastinzi@gmail.com>
2023-10-31 00:20:52 +0200

    dds

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -15,31 +15,21 @@ class OverrideMetaClass(type):
             if attr != '__init_subclass__':
                 n_attributes.append(attr)
         return n_attributes
-
+"""
+This is a a creation of BaseGeometry 
+"""
 class BaseGeometry(metaclass=OverrideMetaClass):
     """
-    This class models an empty class
-    """
-    def __dir__(cls) -> None:
-        """
-        control access to some inherited attributes
-        """
-        attributes = super().__dir__()
-        n_attributes = []
-        for attr in attributes:
-            if attr != '__init_subclass__':
-                n_attributes.append(attr)
-        return n_attributes
+    This is a a class with area method and integer validator
+    integer validator args(value,name)
+    These function must raise an exception 
+    """ 
     
     def area(self):
-        """a method to raise an exception with a message"""
         raise Exception("area() is not implemented")
-    
     def integer_validator(self, name, value):
-        """
-        A method that validates value
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        name = ""
+        if type(value) != int:
+            raise Exception(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be grea
+            raise Exception(f"{name} must be greater than 0")
