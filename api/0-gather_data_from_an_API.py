f2b0b244f80bc5bc2f032bd246349b68e3f21763
Gatsinzimeek <hackergastinzi@gmail.com>
2023-11-02 09:57:44 +0200

    python Api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,49 +1,28 @@
 """
-This script uses an API to retrieve employee task information
-and display in a special format.
-
-It retrieves employees name, task completed with their titles.
+Python Script that uses the jsonplaceholder REST API
 """
-import requests
-import sys
-
-# No execution of this file when imported
 if __name__ == "__main__":
-    
-# Pass employee id on command line
-    id = sys.argv[1]
+    import requests
+    from sys import argv
 
-# APIs 
-    userTodoURL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    userProfile = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user_id = argv[1]
+    r_user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    )
+    r_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    )
 
-# Make requests on APIs
-    todoResponse = requests.get(userTodoURL)
-    profileResponse = requests.get(userProfile)
+    tasks = r_todos.json()
+    completed_tasks_titles = [
+        task["title"] for task in tasks if task["completed"]
+    ]
 
-# Parse responses and store in variables
-    todoJson_Data = todoResponse.json()
-    profileJson_Data = profileResponse.json()
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            r_user.json()["name"], len(completed_tasks_titles), len(tasks)
+        )
+    )
 
-#Get employee information
-    employeeName = profileJson_Data['name']
-
-# Count total and completed tasks
-    totalTasks = 0
-    completedTasks = 0
-
-    for data in todoJson_Data: # Each dict in variable data
-        for key, value in data.items():
-            if key == 'completed':
-                totalTasks += 1
-                if value == True:
-                    completedTasks += 1
-
-    print("Employee {} is done with "
-    "tasks({}/{}):".format(employeeName, completedTasks, totalTasks))
-
-# Retrieve title of completed tasks
-    for data in todoJson_Data: # Each dict in variable data
-        for key, value in data.items():
-            if key == 'completed' and value == True:
-                print("\t {}".format (data['title']))
+    for title in completed_tasks_titles:
+        print("\t {}".format(title))
